9845bff7e3e6160fe8f1e4cf9df890cb0eecd5d5
Sefender <51956385+Sefender@users.noreply.github.com>
2019-10-16 17:22:00 +0200

    Visual studio fix
    Signed-off-by: Sefender <51956385+Sefender@users.noreply.github.com>

---
+++ Assets/Experimental/Scripts/FireflyDisband.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflyDisband : MonoBehaviour
+{
+    private GameObject flies;
+    void Start()
+    {
+        flies = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ray eller collision
+    }
+}
